402a365209a49ca2877c40f9e23a0ff9c0dc26c2
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-05-24 17:34:21 +0300

    Add broken language

--- tokenizer.rb
+++ tokenizer.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 
-#17:09
+# FIX OUTPUT
 class Tokenizer
   TOKEN_TYPES = [
     [:def, /\bdef\b/],
@@ -9,7 +9,8 @@ class Tokenizer
     [:identifier, /\b[a-zA-Z]+\b/],
     [:integer, /\b[0-9]+\b/],
     [:oparen, /\(/],
-    [:cparen, /\)/]
+    [:cparen, /\)/],
+    [:comma, /,/]
   ]
 
   def initialize(code)
@@ -59,7 +60,36 @@ class Parser
   end
 
   def parse_expr
-    parse_integer
+    if peek(:integer)
+      parse_integer
+    elsif peek(:identifier) && peek(:oparen, 1)
+      parse_call
+    else
+      parse_var_ref
+    end
+  end
+
+  def parse_call
+    name = consume(:identifier)
+    arg_exprs = parse_arg_exprs()
+    CallNode.new(name, arg_exprs)
+  end
+
+  def parse_arg_exprs
+    arg_exprs = []
+
+    consume(:oparen)
+
+    if !peek(:cparen)
+      arg_exprs << parse_expr
+      while peek(:comma)
+        consume(:comma)
+        arg_names << parse_expr
+      end
+    end
+
+    consume(:cparen)
+    arg_exprs
   end
 
   def parse_integer
@@ -67,9 +97,23 @@ class Parser
   end
 
   def parse_arg_names
+    arg_names = []
+
     consume(:oparen)
+
+    if peek(:identifier)
+      arg_names << consume(:identifier).value
+      while peek(:comma)
+        consume(:comma)
+        arg_names << consume(:identifier)
+      end
+    end
     consume(:cparen)
-    []
+    arg_names.values
+  end
+
+  def parse_var_ref
+    VarRefNode.new(consume(:identifier).value)
   end
 
   def consume(expected_type)
@@ -80,12 +124,42 @@ class Parser
       raise "Expected token type #{expected_type.inspect} but got #{token.type.inspect}"
     end
   end
+
+  def peek(expected_type, offset=0)
+    @tokens.fetch(offset).type == expected_type
+  end
+end
+
+class Generator
+  def generate(node)
+    case node
+    when DefNode
+      "function %s(%s) {returns %s}; " % [
+        node.name,
+        node.arg_names.join(','),
+        generate(node.body)
+      ]
+    when CallNode
+      "%s(%s)" % [
+        node.name,
+        node.arg_exprs.map { |e| generate(e) }.join(",")
+      ]
+    when VarRefNode
+      node.value
+    when IntegerNode
+      node.value
+    else raise "unexpected #{node.class}"
+    end
+  end
 end
 
 DefNode = Struct.new(:name, :arg_names, :body)
 IntegerNode = Struct.new(:value)
-
+CallNode = Struct.new(:name, :arg_exprs)
+VarRefNode = Struct.new(:value)
 tokens = Tokenizer.new(File.read('test.src')).tokenize
 puts tokens.map(&:inspect).join("\n")
 tree = Parser.new(tokens).parse
-p tree
+# puts tree
+generated = Generator.new.generate(tree)
+puts generated
